754f77d8c28b45df16aa1a1c7742cbfc9313d90f
서재원 <cinos81@nhnlink.co.kr>
2024-08-27 10:13:29 +0900

    기본적인 react + i18next 사용법

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18next.init({
-    lng: 'ko',
-    resources: {
-        en: {
-            translation: {
-                "환영합니다": "Welcome"
+i18next
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        lng: 'ko',
+        resources: {
+            en: {
+                translation: {
+                    "환영합니다": "Welcome"
+                }
             }
         }
-    }
-})
-
-console.log(i18next.t('환영합니다', { ns: 'en' }));
+    })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
