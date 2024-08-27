5491f978934a25375064584d16638f565d820c2a
서재원 <cinos81@nhnlink.co.kr>
2024-08-28 08:55:52 +0900

    i18next-parser 설정에러를 수정

--- translation/i18next-parser.config.js
+++ translation/i18next-parser.config.js
@@ -1,6 +1,6 @@
 export default {
-    src: [
-        'src/**/*.{ts,tsx}', // typescript 코드에서 번역 키를 검색
+    input: [
+        '../src/**/*.{ts,tsx}', // typescript 코드에서 번역 키를 검색
     ],
     output: 'public/locales/$LOCALE/$NAMESPACE.json',
     defaultNamespace: 'translation',
