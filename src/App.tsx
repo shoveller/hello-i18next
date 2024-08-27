a67ce6ba225b22e54bad08f61c1408558ebceeb2
서재원 <cinos81@nhnlink.co.kr>
2024-08-27 17:07:03 +0900

    i18next-parser 로 소스코드를 분석하는 코드를 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
-import { FC } from "react";
 
 const useTransationFromUrl = () => {
     const { t } = useTranslation()
@@ -10,41 +9,38 @@ const useTransationFromUrl = () => {
     return (text: string) => t(text, {lng: language})
 }
 
-const T: FC<{ children: string }> = ({ children }) => {
-    const t = useTransationFromUrl()
-
-    return <>{t(children)}</>
-}
-
 function App() {
-  const navigate = useNavigate()
-  const t = useTransationFromUrl()
-  const { i18n } = useTranslation()
+    const navigate = useNavigate()
+    const t = useTransationFromUrl()
+    const {i18n} = useTranslation()
 
-  return (
-    <div>
-      <div>
-        <button onClick={async () => {
-            await i18n.changeLanguage('ko');
-            navigate('/ko');
-        }}>한국어</button>
-        <button onClick={async () => {
-            await i18n.changeLanguage('en');
-            navigate('/en');
-        }}>영어</button>
-        <button onClick={async () => {
-            await i18n.changeLanguage('ja');
-            navigate('/ja');
-        }}>일본어</button>
-        <button onClick={async () => {
-            await i18n.changeLanguage('zh');
-            navigate('/zh');
-        }}>중국어</button>
-      </div>
-      <>{t('환영합니다')}</>
-      <T>환영합니다</T>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <button onClick={async () => {
+                    await i18n.changeLanguage('ko');
+                    navigate('/ko');
+                }}>한국어
+                </button>
+                <button onClick={async () => {
+                    await i18n.changeLanguage('en');
+                    navigate('/en');
+                }}>영어
+                </button>
+                <button onClick={async () => {
+                    await i18n.changeLanguage('ja');
+                    navigate('/ja');
+                }}>일본어
+                </button>
+                <button onClick={async () => {
+                    await i18n.changeLanguage('zh');
+                    navigate('/zh');
+                }}>중국어
+                </button>
+            </div>
+            <>{t('환영합니다')}</>
+        </div>
+    )
 }
 
 export default App
--- src/i18n.ts
+++ src/i18n.ts
@@ -7,19 +7,20 @@ i18next
     .use(HttpBackend)
     .use(LanguageDetector)
     .use(initReactI18next).init({
-        lng: 'ko',
-        fallbackLng: 'ko',
-        supportedLngs: ['ko', 'en', 'ja', 'zh'],
-        detection: {
-            // path variable 만 사용
-            order: ['path'],
-            // 경로의 첫 번째 부분을 언어로 감지
-            lookupFromPathIndex: 0,
-            // localStorage, cookie, sessionStorage 등에 언어 정보를 저장하지 않도록 함
-            caches: []
-        },
-        backend: {
-          // 리소스 파일의 경로를 지정
-          loadPath: '/locales/{{lng}}/{{ns}}.json',
-        }
-    })
+    debug: true,
+    lng: 'ko',
+    fallbackLng: 'ko',
+    supportedLngs: ['ko', 'en', 'ja', 'zh'],
+    detection: {
+        // path variable 만 사용
+        order: ['path'],
+        // 경로의 첫 번째 부분을 언어로 감지
+        lookupFromPathIndex: 0,
+        // localStorage, cookie, sessionStorage 등에 언어 정보를 저장하지 않도록 함
+        caches: []
+    },
+    backend: {
+        // 리소스 파일의 경로를 지정
+        loadPath: '/locales/{{lng}}/{{ns}}.json',
+    }
+})
