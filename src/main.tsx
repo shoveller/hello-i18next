f7ab04082bb736e8d9407e9735b47d6770dc7199
서재원 <cinos81@nhnlink.co.kr>
2024-08-27 10:00:53 +0900

    기본적인 i18next 사용법

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import i18next from "i18next";
+
+i18next.init({
+    lng: 'ko',
+    resources: {
+        en: {
+            translation: {
+                "환영합니다": "Welcome"
+            }
+        }
+    }
+})
+
+console.log(i18next.t('환영합니다', { ns: 'en' }));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
